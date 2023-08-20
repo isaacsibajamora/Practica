8f992ccf9c33b13eaf7226282ac8a459d526b97a
isaacsibajamora <isaacsibaja901@gmail.com>
2023-08-20 14:30:46 -0600

    se crea clase pepe
     creando la clases

---
+++ src/prac/pepe.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prac;
+
+/**
+ *
+ * @author Isaac Sibaja
+ */
+public class pepe {
+    private int telefono;
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public pepe(int telefono, String nombre, String apellido, String correo) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
